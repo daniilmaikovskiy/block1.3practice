4ebaae374aa9fa9ebc7785a1d3fa96a88a5bf9dd
maikovsky danil <maikovsky.danil@gmail.com>
2020-05-09 16:35:59 +0300

    click listeners updated

--- js/block1.5.js
+++ js/block1.5.js
@@ -1,17 +1,16 @@
-// look q and qAll functions in trueQuerySelector.js
+const HIDDENS_ON_MIDDLE_SCREEN = [7, 8];
+const FIRST_HIDDEN_ON_LARGE_SCREEN = 9;
 
+// look q and qAll functions in trueQuerySelector.js
 let brandBlocks = qAll('.brand-block');
 let showButton = q('#show-button');
 let hideButton = q('#hide-button');
 
-hideSomeBrandBlocks(brandBlocks);
-addShowButtonClickEvent(showButton, hideButton);
-addHideButtonClickEvent(showButton, hideButton);
+addHiddenModifiers(brandBlocks);
+addShowButtonClickEvent(showButton, hideButton, brandBlocks);
+addHideButtonClickEvent(showButton, hideButton, brandBlocks);
 
-function hideSomeBrandBlocks(brandBlocks) {
-    const HIDDENS_ON_MIDDLE_SCREEN = [7, 8];
-    const FIRST_HIDDEN_ON_LARGE_SCREEN = 9;
-
+function addHiddenModifiers(brandBlocks) {
     for (let i = HIDDENS_ON_MIDDLE_SCREEN[0] - 1; 
         i < HIDDENS_ON_MIDDLE_SCREEN[HIDDENS_ON_MIDDLE_SCREEN.length - 1]; i++) {
     
@@ -24,16 +23,31 @@ function hideSomeBrandBlocks(brandBlocks) {
     }
 }
 
-function addShowButtonClickEvent(showButton, hideButton) {
+function removeHiddenModifiers (brandBlocks) {
+    for (let i = HIDDENS_ON_MIDDLE_SCREEN[0] - 1; 
+        i < HIDDENS_ON_MIDDLE_SCREEN[HIDDENS_ON_MIDDLE_SCREEN.length - 1]; i++) {
+    
+        brandBlocks[i].classList.remove('brand-block--hidden@middle-screen');
+    }
+
+    for (let i = FIRST_HIDDEN_ON_LARGE_SCREEN - 1; i < brandBlocks.length; i++) {
+        
+        brandBlocks[i].classList.remove('brand-block--hidden@middle-large-screen');
+    }
+}
+
+function addShowButtonClickEvent(showButton, hideButton, brandBlocks) {
     showButton.addEventListener('click', function () {
         hideButton.classList.remove('show-hide-button--hidden');
         showButton.classList.add('show-hide-button--hidden');
+        removeHiddenModifiers(brandBlocks);
     });
 }
 
-function addHideButtonClickEvent(showButton, hideButton) {
+function addHideButtonClickEvent(showButton, hideButton, brandBlocks) {
     hideButton.addEventListener('click', function () {
         showButton.classList.remove('show-hide-button--hidden');
         hideButton.classList.add('show-hide-button--hidden');
+        addHiddenModifiers(brandBlocks);
     });
 }
