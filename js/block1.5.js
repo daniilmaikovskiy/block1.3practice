b70f7fd3d2c5681374bfc6a6af615a96dd54fbd4
maikovsky danil <maikovsky.danil@gmail.com>
2020-05-10 15:31:55 +0300

    swiper added

--- js/block1.5.js
+++ js/block1.5.js
@@ -1,11 +1,24 @@
 const HIDDENS_ON_MIDDLE_SCREEN = [7, 8];
 const FIRST_HIDDEN_ON_LARGE_SCREEN 
     = HIDDENS_ON_MIDDLE_SCREEN[HIDDENS_ON_MIDDLE_SCREEN.length - 1] + 1;
+const MIN_WIDTH_MIDDLE_SCREEN = 768;
 
 // look q and qAll functions in trueQuerySelector.js
-let brandBlocks = qAll('.brand-block');
+let brandBlocks = qAll('.brand-block', '.menu--hidden\\@small-screen');
 let showButton = q('#show-button');
 let hideButton = q('#hide-button');
+let swiper = new Swiper ('.swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    spaceBetween: 16,
+    grabCursor: true,
+    slidesOffsetBefore: 16,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+});
 
 addHiddenModifiers(brandBlocks);
 addShowButtonClickEvent(showButton, hideButton, brandBlocks);
